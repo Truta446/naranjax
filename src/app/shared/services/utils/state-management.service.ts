3fdc6b2392675f5e0dfba4d59d4cb7eea926a7fd
Truta446 <30662756+Truta446@users.noreply.github.com>
2025-01-03 13:43:48 +0000

    Deploying to gh-pages from @ Truta446/naranjax@fe65b7a8a9af76f23e00d834ca6186f22e899eeb 🚀

--- src/app/shared/services/utils/state-management.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class StateManagementService {
-  public isLoading: WritableSignal<boolean> = signal(false);
-}
